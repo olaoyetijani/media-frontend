11253c2e908de52f8d9f2252f0944fc6a913dd90
Hussein Sheu Tijani <adeolatijani12@gmail.com>
2024-01-06 08:44:48 +0100

    mui

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,16 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import { Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth/Auth";
-import { ThemeProvider } from "@mui/styles";
-import { createTheme } from "@mui/material";
 
 function App() {
-  const theme = createTheme();
-
   return (
-    <ThemeProvider theme={theme}>
-      <Container maxWidth="lg">
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-        </Routes>
-      </Container>
-    </ThemeProvider>
+    <Container maxWidth="lg">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+    </Container>
   );
 }
 
