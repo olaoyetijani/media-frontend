61625acbcd24fb54a865697fc077cd2053a954a9
Hussein Sheu Tijani <adeolatijani12@gmail.com>
2024-01-06 13:33:38 +0100

    responsiveness

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,18 +22,20 @@ const Home = () => {
           container
           justifyContent="space-between"
           alignItems={"stretch"}
-          style={{ display: 'flex', flexDirection: 'row'}}
+          style={{ display: "flex", flexDirection: "row" }}
           sx={{
             flexDirection: {
               sm: "column-reverse",
             },
+            my: 5
           }}
         >
-          <Grid item xs={12} sm={7}>
-            <Posts setCurrentId={setCurrentId} />
-          </Grid>
           <Grid item xs={12} sm={4}>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
           </Grid>
         </Grid>
       </Container>
