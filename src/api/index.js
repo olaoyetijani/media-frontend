5b97e19fe1e3e86097137e8dc515e57c6250baa7
Hussein Sheu Tijani <adeolatijani12@gmail.com>
2024-01-07 07:39:25 +0100

    API changed

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,11 @@
 import axios, { formToJSON } from "axios";
 
+// const url = "https://media-backend-xxg4.onrender.com/posts";
+// const userUrl = "https://media-backend-xxg4.onrender.com/user";
+
 const API = axios.create({
   baseURL: "https://media-backend-xxg4.onrender.com",
 });
-
-//https://media-backend-xxg4.onrender.com
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
@@ -25,3 +26,13 @@ export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 
 export const signIn = (FormData) => API.post("/user/signin", FormData);
 export const signUp = (FormData) => API.post("/user/signup", FormData);
+
+// export const fetchPosts = () => axios.get(url);
+// export const createPosts = (newPost) => axios.post(url, newPost);
+// export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+// export const updatePost = (id, updatedPost) =>
+//   axios.patch(`${url}/${id}`, updatedPost);
+// export const deletePost = (id) => axios.delete(`${url}/${id}`);
+
+// export const signIn = (FormData) => axios.post(`${userUrl}/signin`, FormData);
+// export const signUp = (FormData) => axios.post(`${userUrl}/signup`, FormData);
